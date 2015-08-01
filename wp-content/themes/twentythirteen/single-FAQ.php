9c0d5a9a9594590057b2587a4d0d35af53271fc5
mostafaolama <mostafaolama111@gmail.com>
2015-08-01 19:57:49 +0430

    q

---
+++ wp-content/themes/twentythirteen/single-FAQ.php
@@ -0,0 +1,19 @@
+<?php
+/* Template Name: 'FAQ'  */
+
+
+get_header();
+
+	$args = array(
+	'post_type' => 'FAQ' 
+	); 
+	$query = new WP_Query( $args );
+		while ($query->have_posts()) : $query->the_post(); 
+?>	 <h2 dir="rtl"><?=the_title()?> </h2>
+		<h4 dir="rtl"><?=the_content() ?></h4>
+		<br> 
+<?php
+		endwhile ; 
+		
+get_footer();
+?>
